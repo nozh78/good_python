6786927d3571596f0b4ebdc217ff67c0d07fa416
Evgeny Novikov <verno72@mail.ru>
2023-12-20 22:37:39 +0300

    Подвиг 3

--- 5/5_2_21.py
+++ 5/5_2_21.py
@@ -1,10 +1,29 @@
 # Подвиг 2
-p = [0]*10
+# p = [0] * 10
+# count = 0
+# num = 0
+# while count < 7:
+#     num = int(input())
+#     if p[num] == 0:
+#         p[num] = 1
+#     count += 1
+# print(*p)
+
+
+# Подвиг 3
+# На каждой итерации цикла вводится целое число. Необходимо подсчитать произведение
+# только положительных чисел, до тех пор, пока не будет введено значение 0.
+# Реализовать пропуск вычислений с помощью оператора continue,
+# а также использовать цикл while. Результат произведения вывести на экран.
+proizv = 1
 count = 0
-num = 0
-while count < 7:
-    num = int(input())
-    if p[num] == 0:
-        p[num] = 1
+while count < 10:
+    chislo = int(input("Введите число "))
+    if chislo > 0:
+        proizv *= chislo
+    elif chislo == 0:
+        break
+    elif chislo < 0:
+        continue
     count += 1
-print(*p)
+print(proizv)

4f343c094a20423292be04f3c736f009ce0860db
Evgeny Novikov <verno72@mail.ru>
2023-12-21 14:36:48 +0300

    Подвиг 4

--- 5/5_2_21.py
+++ 5/5_2_21.py
@@ -15,15 +15,33 @@
 # только положительных чисел, до тех пор, пока не будет введено значение 0.
 # Реализовать пропуск вычислений с помощью оператора continue,
 # а также использовать цикл while. Результат произведения вывести на экран.
-proizv = 1
-count = 0
-while count < 10:
-    chislo = int(input("Введите число "))
-    if chislo > 0:
-        proizv *= chislo
-    elif chislo == 0:
-        break
-    elif chislo < 0:
-        continue
-    count += 1
-print(proizv)
+# proizv = 1
+# count = 0
+# while count < 10:
+#     chislo = int(input("Введите число "))
+#     if chislo > 0:
+#         proizv *= chislo
+#     elif chislo == 0:
+#         break
+#     elif chislo < 0:
+#         continue
+#     count += 1
+# print(proizv)
+
+
+# Подвиг 4.
+# Вводится список названий городов в одну строчку через пробел. Определить, что в
+# этом списке все города имеют длину более 5 символов. Реализовать программу с
+# использованием цикла while и оператора break. Вывести ДА, если условие выполняется
+#  и НЕТ - в противном случае.
+# cities = input()
+# cities_list = cities.split()
+# count = 0
+# while count < len(cities_list):
+#     if len(cities_list[count]) > 5:
+#         count += 1
+#     else:
+#         print("НЕТ")
+#         break
+# else:
+#     print("ДА")
